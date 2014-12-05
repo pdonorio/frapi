e374466d010cf08f3c987b716d74acf873065fdd
pdonorio <p.donorio.demeo@gmail.com>
2014-12-05 12:14:01 +0100

    Factory and double promise.

--- frangui/js/controllers.js
+++ frangui/js/controllers.js
@@ -1,62 +1,30 @@
 
 //###########################################
 //controller
-myModule.controller('TableController', ['$scope', 'Restangular',
-  function($scope, Restangular) {
+myModule.controller('TableController', function($scope, DataResource) {
 
     //INIT
     $scope.datacount = 0;
     $scope.data = {};
     $scope.headers = [ "", "Key", "Value" ];
 
-    // All URLs on searches will use `http://google.com/` as the baseUrl
-    api = Restangular.allUrl('pyapi','http://awesome.dev:5507');
-    api.customGET("data").then(function(output) {
-      data = JSON.parse(output);
-      //console.log(data);
-      $scope.datacount = data.count;
-      $scope.data = data.items;
-    });
+    promise = DataResource.get("data");
 
+    promise
+      //Success
+      .then(function(data) {
+        $scope.datacount = data.count;
+        $scope.data = data.items;
+      },
+      //Error
+      function(object) {
+        console.log("Controller api call Error");
+        console.log(object);
+      }
+    );
 
-/*
-
-    //make the call to Api get - use promises
-    Api.get().then(function(result) {
-       //this will execute when the AJAX call completes.
-        console.log(result);
-        $scope.datacount = result.data.count
-        $scope.data = result.data.items;
-// # TO FIX -
-        //what to do if status > 0 (== error)?
-    });
-
-*/
-
-/* FROM THE EXAMPLE.....
-
-    //add
-    $scope.addTodo = function() {
-      $scope.todos.push({text:$scope.todoText, done:false});
-      $scope.todoText = '';
-    };
-    //count
-    $scope.remaining = function() {
-      var count = 0;
-      angular.forEach($scope.todos, function(todo) {
-        count += todo.done ? 0 : 1;
-      });
-      return count;
-    };
-    //archive
-    $scope.archive = function() {
-      var oldTodos = $scope.todos;
-      $scope.todos = [];
-      angular.forEach(oldTodos, function(todo) {
-        if (!todo.done) $scope.todos.push(todo);
-      });
-    };
-*/
+    // DEBUG factory result
+    //console.log(promise);
 
     //end
-  }]);
+});
--- frangui/js/services.js
+++ frangui/js/services.js
@@ -1,69 +1,39 @@
-/*
-
 // Inject Restangular class to use Apis
-myModule.factory('Api', function(Restangular)
-{
+myModule.factory('DataResource', function(Restangular) {
 
-      // All URLs on searches will use `http://google.com/` as the baseUrl
-      var searches = Restangular.allUrl('searches', 'http://awesome.dev:5507/');
-      searches.get().then(function(results) {
-        console.log(results);
-      });
-      return {};
+    //###################################
+    //Empty factory
+    var Factory = {};
 
-/*
-    return {
+    // All URLs on searches will use `http://google.com/` as the baseUrl
+    var api = Restangular.allUrl('pyapi','http://awesome.dev:5507');
 
+    //###################################
+    //GET DATA
+    Factory.get = function(resource) {
 
-      //####################################
-      // GET data from API
-      get: function() {
+      //console.log(resource);
 
-        return searches.get()
-          .then(function(results) {
-            console.log(results);
-            return {};
-            /*
-            return {
-              "status": 0,
-              "message": "OK",
-              "data": {}, //JSON.parse(result.data),
-            };
+      // Make a promise for data call
+      promise = api.customGET(resource)
+        .then(
+          //Success
+          function(output) {
+            data = JSON.parse(output);
+            //console.log(data);
+            return data;
+          },
+          //Error
+          function(object) {
+            console.log("Factory/Service api call Error");
+            console.log(object);
           }
+        );
+      return promise;
 
-        )}
+    };
 
-//# TO FIX - i should return some better standard for results,
-//together with the api response (which must be improved)
-
-/*
-        // Simple GET request example :
-        return $http.get('http://awesome.dev:5507/data')
-          .then(function(result) {
-              //http is serializing my data, so parse Json before giving it back
-              return {
-                "status": 0,
-                "message": "OK",
-                "data": JSON.parse(result.data),
-              };
-           },
-          //and what about errors?
-            function(data) {
-              // Handle error here
-              //console.log(data);
-              return {
-                "status": data.status,
-                "message": data.statusText,
-                "data": {},
-              }
-            });
-
-
-      }
-
-
-      //####################################
-      // POST data to API ?
+    //###################################
+    return (Factory);
 
 });
-*/
