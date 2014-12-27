742be14b767276c5bb320b1be9ebd01f713b7e86
pdonorio <p.donorio.demeo@gmail.com>
2014-12-27 20:56:00 +0100

    Putting ideas

--- yoapp/app/scripts/controllers/main.js
+++ yoapp/app/scripts/controllers/main.js
@@ -12,7 +12,9 @@ myApp
   {
     $scope.menu = [
         {active:true, link:'', name:'home'},
-        {active:false, link:'search', name:'data'},
+        {active:false, link:'about', name:'submit'},
+        {active:false, link:'search', name:'search'},
+        {active:false, link:'about', name:'changelog'},
         {active:false, link:'about', name:'about'},
     ];
 
