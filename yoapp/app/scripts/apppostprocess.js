b51ee9a8b15a44d5343cc98e1449977345b22627
pdonorio <p.donorio.demeo@gmail.com>
2015-01-12 20:20:28 +0100

    Fixed bug of strange error Uncaught SyntaxError: Unexpected token <

--- yoapp/app/scripts/apppostprocess.js
+++ yoapp/app/scripts/apppostprocess.js
@@ -6,6 +6,6 @@ myApp.run(function(editableOptions)
 {
     //console.log("app run");
 
-    //Uhm: which is this library??
-    editableOptions.theme = 'bs3'; // bootstrap3 theme. Can be also 'bs2', 'default'
+    // bootstrap3 theme. Can be also 'bs2', 'default'
+    editableOptions.theme = 'bs3';
 });
