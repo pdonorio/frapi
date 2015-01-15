27ddf7a294ffd8db0cb43ccf7ddef6616ea2fad8
pdonorio <p.donorio.demeo@gmail.com>
2015-01-15 17:19:53 +0100

    Moved data init for steps from directive to main controller for that view. Much cleaner. Should try some json mocks for init... Should try reading json files or the python mock?

--- yoapp/app/scripts/controllers/submissioncontroller.js
+++ yoapp/app/scripts/controllers/submissioncontroller.js
@@ -8,33 +8,105 @@
  * Controller of the yoApp
  */
 myApp
-  .controller('SubmissionController', function ($scope) {
+  .controller('SubmissionController',
+    ['$scope', '$filter',
+    function ($scope, $filter)
+  {
 
+    //First step
     $scope.current = 1;
-
-    $scope.formTemplate = [
-            {pos:1, key: "titolo", type: "number"},
-            {pos:2, key: "pippo", type: "select"},
-            {pos:6, key: "ancora", type: "text"},
-            {pos:4, key: "ultimo", type: "textarea"},
-        ];
-
-    $scope.steps = [
-        {step: 1, name: "estratto", form: [
-            {pos:1, key: "titolo", value: 4},
-            {pos:4, key: "ultimo", value: "test"},
-            {pos:3, key: "ancora", value: "prova"},
-        ] },
-        {step: 2, name: "fonte", form: []},
-        {step: 3, name: "festa", form: []},
-        {step: 4, name: "altro", form: []},
-    ];
 
     //define step on click
     $scope.setStep = function(step) {
         $scope.current = step;
     }
 
-    //$scope.init = function(tmp) { console.log("My init" + tmp); }
+    //////////////////////////////////////////////
+    // SETUP data - read from API
+/*
+//TO FIX:
+* 1 - there should be one template per each step
+* 2 - read from api: /step_template/#STEP_NUM
+*/
+    $scope.formTemplate =
+        //step: 1, template:
+        [
+            {pos:1, key: "titolo", type: "number"},
+            {pos:2, key: "pippo", type: "select"},
+            {pos:3, key: "ancora", type: "text"},
+            {pos:4, key: "ultimo", type: "textarea"},
+        ];
 
-  });
+    $scope.steps = [
+        {step: 1, name: "estratto",
+            form: [
+                {pos:1, key: "titolo", value: 3},
+                {pos:4, key: "ultimo", value: "test"},
+                {pos:3, key: "ancora", value: "<i>html</i>"},
+                {pos:2, key: "pippo", value: "se moi"},
+            ]
+        },
+        {step: 2, name: "fonte",
+            form: [
+                {pos:1, key: "titolo", value: 4},
+                {pos:4, key: "ultimo", value: "test"},
+                {pos:3, key: "ancora", value: "prova"},
+            ]
+        },
+        {step: 3, name: "festa",
+            form: [
+                {pos:1, key: "titolo", value: 1},
+                {pos:3, key: "ancora", value: "test"},
+                {pos:2, key: "pippo", value: "riprova"},
+            ]
+        },
+        {step: 4, name: "altro",
+            form: [
+                {pos:1, key: "titolo", value: 4},
+                {pos:4, key: "ultimo", value: "test"},
+                {pos:3, key: "ancora", value: "prova"},
+            ]
+        },
+    ];
+
+    //////////////////////////////////////////////
+    // INIT EACH STEP
+
+    $scope.bootData = [];
+
+    for (var x = 0; x < $scope.steps.length; x++)
+    {
+        var step = $scope.steps[x];
+        //fix data based on template
+        //should be different for each step
+        var template = $scope.formTemplate;
+
+        //otherwise ng-repeat filters DO NOT WORK:
+        var results = [];   //ARRAY and NOT JSON!!!!!
+
+        //foreach template, get data and create new element in scope
+        for (var j = 0; j < template.length; j++)
+        {
+            //get the position
+            var tmp = template[j];
+            var i = tmp.pos;
+
+            //find existing data in that position
+            var data = $filter('filter')(step.form, {pos: i});
+            tmp.value = null;
+
+            //if exists
+            if (data && data.length > 0 && data[0].key == tmp.key) {
+                //mix template with existing data inside results
+                tmp.value = data[0].value;
+            }
+            //save results with key pos orderable
+            results[j] = angular.copy(tmp);
+        };
+
+        //save step x
+        $scope.bootData[x] = angular.copy(results);
+    }
+    //console.log($scope.bootData);
+
+ }]);
--- yoapp/app/scripts/directives/submissionstep.js
+++ yoapp/app/scripts/directives/submissionstep.js
@@ -24,43 +24,14 @@ myApp
         template: "=",
       },
       //manage the local scope
-      controller: ['$scope', '$filter', function($scope, $filter) {
-
-//TO FIX - define a function to update at every STEP CLICK CHANGE
-
-        $scope.init = function(tmp) {
-            console.log(tmp);
-        }
-
+      controller: function($scope)
+      {
+        //console.log($scope.data);
+        console.log("Remove this controller " + ($scope.step + 1) + " ?");
 //TO FIX -
         //?? NON FUNZIONA - chiedere a autore di xeditable
         //console.log($scope.myform);
-
-        //fix data based on template
-        //foreach template, get data and create new element in scope
-        var results = [];   //ARRAY and NOT JSON!!!!!
-        //otherwise ng-repeat filters DO NOT WORK
-
-        for (var j = 0; j < $scope.template.length; j++)
-        {
-            //get the position
-            var tmp = $scope.template[j];
-            var i = tmp.pos;
-
-            //find existing data in that position
-            var data = $filter('filter')($scope.data, {pos: i});
-            tmp.value = null;
-            //if exists
-            if (data && data.length > 0 && data[0].key == tmp.key) {
-                //mix template with existing data inside results
-                tmp.value = data[0].value;
-            }
-            //save results with key pos orderable
-            results[j] = angular.copy(tmp);
-        };
-        $scope.data = results;
-      }],
-
+      },
 /*
       //manipulate DOM
       link : function(scope, element, attrs, ctrl) {
