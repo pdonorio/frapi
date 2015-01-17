4c21f0edc8c93ee3dcd0e1e051a6b95acc82aacb
pdonorio <p.donorio.demeo@gmail.com>
2015-01-17 16:05:57 +0100

    Better handling of timeout for multiple messages

--- yoapp/app/scripts/directives/notify.js
+++ yoapp/app/scripts/directives/notify.js
@@ -12,28 +12,42 @@ myApp
       restrict: 'E',
       templateUrl: 'templates/notification.html',
 
-      //item is my internal object, with status and message
+      // 'item' is my internal object,
+      // with status and message and latest timeout
       controller: function($scope, $timeout, messageTimeout)
       {
+        //init structure
         var empty = {
             status: 0,
             message: "",
+            timeout: null,
         }
-        $scope.item = empty;
+        //do not pass this as a reference...
+        $scope.item = angular.copy(empty);
 
         $scope.setNotification = function(status, message)
         {
-            $scope.item = {
-                status: status,
-                message: message,
-            }
+            $scope.item.message = message;
+            $scope.item.status = status;
+
+            // Handling timeout
             if (status > 1) {
-                $timeout(function()
-                {
-                    //only updating default status to make
-                    //message disappear
-                    $scope.item.status = empty.status;
-                }, messageTimeout);
+                //in some seconds i want message to disappear
+                //should this be optional?
+                $scope.item.timeout = $timeout(function() {
+
+                        //only updating default status to make
+                        //message disappear
+                        $scope.item.status = angular.copy(empty.status);
+                    }, messageTimeout
+                );
+            } else {
+                //if i go to status normal or loading
+                //i have to avoid that an old timeout close my message
+                if ($scope.item.timeout) {
+                    //console.log("Timeout exit");
+                    $timeout.cancel($scope.item.timeout);
+                }
             }
         }
       }
