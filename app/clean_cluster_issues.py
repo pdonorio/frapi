985d8e78b235b8870a177733986d5535adffdeff
pdonorio <p.donorio.demeo@gmail.com>
2015-04-29 06:47:32 +0200

    Adding proxy for rethinkdb monitor. Fixing little code for it.

--- app/clean_cluster_issues.py
+++ app/clean_cluster_issues.py
@@ -1,12 +1,18 @@
+# Load libraries
 import rethinkdb as r
 
 conn = r.connect(host="db", db='rethinkdb')
 
 q = r.table("current_issues").run(conn)
+
+# For each issue
 for i in q:
     s_name = i['info']['disconnected_server']
     print "Failed server: ", s_name
+
+    # Search the original UUID of this server
     cursor = r.table("server_config").filter({'name':s_name}).run(conn)
+    # Only one result from this
     for j in cursor:
         print "Removing", j['id']
         r.table('server_config').get(j['id']).delete().run(conn)
