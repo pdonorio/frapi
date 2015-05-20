96a67a859afddf6fac8b37fb5294c10436510b86
pdonorio <p.donorio.demeo@gmail.com>
2015-05-20 17:24:45 +0200

    Mail conf added: https://pythonhosted.org/Flask-Security/quickstart.html#id4

--- app/testapp.py
+++ app/testapp.py
@@ -2,7 +2,9 @@
 """ A test app for anything i want to try """
 
 from security.roles import app
+from security.mailer import mailer
 
 if __name__ == '__main__':
+    mailer(app)
     app.run(host='0.0.0.0', port=80)
     #app.run(port=4000)
