c083ff57e0244a00f9a746cc45908266e1ef5420
pdonorio <p.donorio.demeo@gmail.com>
2015-05-21 11:07:34 +0200

    Defining a provider and creating a role-oriented token-based auth resource. Tested on normal flask end-points.
    This is the base commit on this branch (**authprovider**)
    for resolving #18.

--- app/security/roles.py
+++ app/security/roles.py
@@ -2,23 +2,39 @@
 """
 A secured app from:
 https://pythonhosted.org/Flask-Security/quickstart.html
+Using the blog post:
+http://mandarvaze.github.io/2015/01/token-auth-with-flask-security.html
 """
 
-from flask import Flask, render_template
-from flask.ext.sqlalchemy import SQLAlchemy
-from flask.ext.security import Security, SQLAlchemyUserDatastore, \
-    UserMixin, RoleMixin, login_required
+####################################
+# Imports
 
+from flask import Flask, jsonify
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.security \
+    import Security, SQLAlchemyUserDatastore, \
+    UserMixin, RoleMixin, login_required, roles_required, \
+    auth_token_required, http_auth_required
+
+####################################
 # Create app
 app = Flask(__name__)
-app.config['DEBUG'] = True
-app.config['SECRET_KEY'] = 'super-secret'
-app.config['SQLALCHEMY_DATABASE_URI'] =  'sqlite://' #'sqlite:///tmp.db'
 
+# Conf
+app.config['DEBUG'] = True
+app.config['WTF_CSRF_ENABLED'] = False
+
+# Security
+app.config['SECRET_KEY'] = 'super-secret'
+# Db in memory for now
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://'
+
+####################################
 # Create database connection object
 db = SQLAlchemy(app)
 
 # Define models
+
 roles_users = \
     db.Table('roles_users',
         db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
@@ -34,23 +50,53 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(255), unique=True)
     password = db.Column(db.String(255))
     active = db.Column(db.Boolean())
+    auth_token = db.Column(db.String(255))
     confirmed_at = db.Column(db.DateTime())
     roles = db.relationship('Role', secondary=roles_users,
                             backref=db.backref('users', lazy='dynamic'))
 
+####################################
 # Setup Flask-Security
 user_datastore = SQLAlchemyUserDatastore(db, User, Role)
 security = Security(app, user_datastore)
+
+USER = 'test@test.it'
+PWD = 'password'
+ROLE_ADMIN = 'adminer'
 
 # Create a user to test with
 @app.before_first_request
 def create_user():
     db.create_all()
-    user_datastore.create_user(email='matt@nobien.net', password='password')
-    db.session.commit()
+    if not User.query.first():
 
+        user_datastore.create_user(email=USER, password=PWD)
+        user_datastore.create_role(name=ROLE_ADMIN, description='I am the admin')
+        #user_datastore.find_or_create_role(ROLE_ADMIN)
+        user_datastore.add_role_to_user(USER, ROLE_ADMIN)
+
+        db.session.commit()
+
+####################################
 # Views
 @app.route('/')
-@login_required
+#@login_required
 def home():
     return "Hello world", 200
+
+@app.route('/app/', methods=['GET'])
+@auth_token_required    #@http_auth_required
+def dummyAPI():
+    ret_dict = {
+        "Key1": "Value1",
+        "Key2": "value2"
+    }
+    return jsonify(items=ret_dict)
+
+@app.route('/admin/')
+@auth_token_required
+@roles_required(ROLE_ADMIN)
+def admin():
+    return "Hello admin", 200
+
+####################################
