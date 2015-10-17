340916fa977f103c9b68f8534e0ef58d1f55e1ac
Paulie D <p.donoriodemeo@gmail.com>
2015-10-17 15:52:36 +0200

    Removing tests on opbeat

--- app/security/config.py
+++ app/security/config.py
@@ -1,12 +1,11 @@
 # -*- coding: utf-8 -*-
 """ Configuration for securing application """
 
-#########################################
-## Read a configuration
-CONFIG_FILE = 'confs/config.ini'
-SECTION = 'opbeat'
-
-import ConfigParser as configparser
+# #########################################
+# ## Read a configuration
+# CONFIG_FILE = 'confs/config.ini'
+# SECTION = 'opbeat'
+# import ConfigParser as configparser
 
 #########################################
 ## Create configurations objects
@@ -45,19 +44,19 @@ class DevelopmentConfig(Config):
     #SECURITY_TOKEN_MAX_AGE = 60
 
 ################
-    ## OPBEAT
-    config = configparser.RawConfigParser() # Make sure configuration is case sensitive
-    config.optionxform = str
-    config.read(CONFIG_FILE)
-    if SECTION not in config.sections():
-        raise Exception("No configuration file for '%s'" % SECTION)
-    # for key, value in config.items(SECTION):
-    #     print(key, value)
-    OPBEAT_ORG = config.get(SECTION, 'org')
-    OPBEAT_APP = config.get(SECTION, 'app')
-    OPBEAT_BEAR = config.get(SECTION, 'bear')
-    OPBEAT_RELURL = config.get(SECTION, 'url').replace('\n', '')
-    ## OPBEAT
+    # ## OPBEAT
+    # config = configparser.RawConfigParser() # Make sure configuration is case sensitive
+    # config.optionxform = str
+    # config.read(CONFIG_FILE)
+    # if SECTION not in config.sections():
+    #     raise Exception("No configuration file for '%s'" % SECTION)
+    # # for key, value in config.items(SECTION):
+    # #     print(key, value)
+    # OPBEAT_ORG = config.get(SECTION, 'org')
+    # OPBEAT_APP = config.get(SECTION, 'app')
+    # OPBEAT_BEAR = config.get(SECTION, 'bear')
+    # OPBEAT_RELURL = config.get(SECTION, 'url').replace('\n', '')
+    # ## OPBEAT
 ################
 
 class TestingConfig(Config):
