e9919475ab0b0605a09871d4b0b84431c2c2ca48
Paulie D <p.donoriodemeo@gmail.com>
2015-10-17 15:26:01 +0200

    A working paging on search with one possibility.

--- yoapp/app/scripts/controllers/search.js
+++ yoapp/app/scripts/controllers/search.js
@@ -3,6 +3,12 @@
 myApp
  .controller('ViewController', function ($scope, $rootScope, $timeout, API, StepContent, perpageDefault, currentpageDefault, focus)
 {
+    $scope.range = function(min, max, step){
+      step = step || 1;
+      var input = [];
+      for (var i = min; i <= max; i += step) input.push(i);
+      return input;
+    };
 
     // Init: Html scope data
     $scope.datacount = 0;
@@ -11,6 +17,7 @@ myApp
     $scope.headers = [ "Operatore", "Estratto", "Altro", "Azioni" ];
     $scope.perpage = perpageDefault;
     $scope.currentpage = currentpageDefault;
+    $scope.pages = [];
 
     // Closed advanced search
     $scope.isCollapsed = true;
@@ -32,7 +39,7 @@ myApp
     {
       if ($scope.selected == lastSelected)
         return;
-      console.log("Searching: ", $scope.selected);
+      //console.log("Searching: ", $scope.selected);
 /*
       console.log($scope.data);
       if (typeof $scope.extra[$scope.selected] !== "undefined")
@@ -76,20 +83,24 @@ myApp
         });
         $scope.typeahead = { data: Object.keys(ext) };
           //'Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California'
-        console.log(ext);
+        //console.log(ext);
         $scope.extra = ext;
     });
 
+
+
     //Bind data in html to function
     $scope.reloadTable = function(perpage, currentpage, searchvalue)
-// TO FIX - enable some cache
+// TO FIX - enable some cache!
     {
+      if (currentpage < 1) currentpage = 1;
+      $scope.currentpage = currentpage;
+      //console.log(perpage, currentpage, "Search", searchvalue);
 
-      console.log("Search", searchvalue);
       // Get the data (as a promise)
       var params = {perpage: perpage, currentpage: currentpage};
 
-// TO FIX - use userlist inside resolve
+// TO FIX - use userlist inside resolve??
       // GET USERS list
       API.get(userresource).then(function(res)
       {
@@ -109,6 +120,8 @@ myApp
 // #######################################
 
           API.get(resource, params).then(function(data) {
+
+                //console.log(data);
 
                 var documents = {}
                 var hashes = {};
@@ -135,12 +148,20 @@ myApp
 
                 // Inject into scope
                 $scope.data = documents;
-                //$scope.datacount = documents.length;
-                $scope.datalength = Object.keys(documents).length;
+                //$scope.datalength = Object.keys(documents).length;
+                $scope.datalength = data.count;
 
                 var from = (parseInt(perpage) * (parseInt(currentpage)-1)) +1;
                 if (from < 1) { from = 1; }
                 $scope.from = from;
+
+                // DEFINE pages array from perpage and currentpage and data size
+                $scope.pages = Math.floor((data.count / $scope.perpage)) + 1;
+                $scope.startpages = $scope.currentpage - 5;
+                if ($scope.startpages < 1) $scope.startpages = 1;
+                $scope.endpages = $scope.startpages + 9;
+                if ($scope.endpages > $scope.pages) $scope.endpages = $scope.pages;
+                //console.log("Pages are", $scope.pages, "for length", data.count);
 
                 // When using this view alone, open the search
                 if (typeof $rootScope.searching === "undefined")
