fe883d3d4dae143f21da663683329f09cc7f7eb1
Paulie D <p.donoriodemeo@gmail.com>
2015-11-07 15:47:56 +0100

    Working with two resultset from rethinkdb, one per table.

--- app/convert.py
+++ app/convert.py
@@ -7,18 +7,57 @@ Convert old schema to new cool and fancy json automatic documents
 from rdb.rdb_handler import RethinkConnection as db
 from rdb.query import RDBquery
 
-mytable = "stepstemplate"
-
 # Connection
 connect = db(True)
 
 # Query Rethinkdb via obj
-query = RDBquery().get_table_query(mytable)
-# Get all table
-data = query.run()
-# ##query = query.get_all(myid, index='id')
-# Debug old
-tmp = list(data)
-print(tmp[0])
+t1 = "stepstemplate"
+t2 = "steps"
+query = RDBquery()
+qt1 = query.get_table_query(t1)
+qt2 = query.get_table_query(t2)
 
-# {u'hash': u'4f73502f', u'extra': None, u'latest_ipaddress': u'217.133.58.250', u'required': u'0', u'field': u'Notes', u'step': 3, u'latest_timestamp': 1429100708.822178, u'position': 7, u'type': 0, u'id': u'1cd435fd-d37c-44e7-b981-40f4a582c917'}
+############################
+# ## QUERIES
+data = qt1.group("step").run()
+for step in list(data):
+    new = {"step": None, "fields": None}
+    myfilter = {'step': step}
+    print("*** STEP: %s" % step)
+
+    # Single step elements
+    element = list(qt2.filter(myfilter).run()).pop()
+    new['step'] = {"num": step, "name": element['label'],
+                   "desc": element['description']}
+
+    # Singles steps fields
+    tmp = []
+    fields = list(qt1.filter(myfilter).run())
+    sorted_fields = sorted(fields, key=lambda k: k['position']) 
+    for row in sorted_fields:
+        print(row['position'])
+        if 'extra' not in row:
+            row['extra'] = None
+        tmp.append({
+            "name": row['field'],
+            "position": row['position'],
+            "required": row['required'],
+            "type": row['type'],
+            "options": row['extra'],
+        })
+    new["fields"] = tmp
+
+    print(new)
+# ## QUERIES
+############################
+
+
+"""
+"fields": [
+    {"name": "a num", "position": 4, "type": "int", "required": 1},
+    {"name": "a list", "position": 1, "type": "list",
+        "options": ["one",2,"three"] },
+    {"name": "a field", "position": 3, "type": "str"},
+    {"name": "default", "position": 2}
+],
+"""
